270e8fb398c44c87dc6d719b20d0a0a32755a817
Roma Bulani <roma.bulani@vista.com>
2024-11-18 19:02:23 +0530

    feat: added vercel file

--- src/index.ts
+++ src/index.ts
@@ -39,5 +39,4 @@ app.post("/reset-google-sync-flag", verifyAuth, resetGoogleSyncFlag);
 
 
 
-const PORT = process.env.PORT ?? 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+export default app;
